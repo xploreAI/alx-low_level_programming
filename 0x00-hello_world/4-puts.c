06a6d074763b0841e326debbd33751f85ff62308
xplorea1 <xplorre.ai@gmail.com>
2022-04-07 18:03:57 +0100

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  *
  * Return: 0 after printing the function
  */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
