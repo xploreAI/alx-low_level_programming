ded24a4e1b272369069bd99c0eeee4b4e1b87509
xplorea1 <xplorre.ai@gmail.com>
2022-04-08 18:31:52 +0100

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
@@ -26,6 +27,7 @@ else
 {
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 }
+
 
 return (0);
 }
